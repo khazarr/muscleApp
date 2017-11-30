b57cdbcda35a8ca2a06709b1a05c666a7a2e997b
Karol Swieca <swieca.karol@gmail.com>
2017-11-30 14:49:07 +0100

    break timer added

--- src/pages/Timer.vue
+++ src/pages/Timer.vue
@@ -18,6 +18,11 @@
         <v-ons-row>
             <v-ons-col>Total workout time <span v-html="totalWorkoutTime">00:00</span></v-ons-col>
         </v-ons-row>
+        <v-ons-button modifier="large" class="button-margin" @click="onNextWorkoutStep">Next workout step</v-ons-button>
+        <v-ons-button v-if="!breakStarted" modifier="large" class="button-margin" @click="onBreakStart">Break time!</v-ons-button>
+        <v-ons-row>
+            <v-ons-col><span v-if="breakStarted" v-html="breakTimeDisplay">00:00</span></v-ons-col>
+        </v-ons-row>
     </section>
 
 
@@ -34,7 +39,18 @@ export default {
           totalWorkoutTime: '00:00',
           totalWorkoutSeconds: 0,
           totalWorkoutMinutes: 0,
-          totalWorkoutTimeout: null,
+          totalWorkoutInterval: null,
+          totalWorkoutStarted: false,
+          breakTimeSeconds: 5,
+          breakTimeMinutes: 0,
+          breakTimeDisplay: '01:00',
+          breakStarted: false,
+          breakInterval: null,
+          workoutStepTimeArray : [{
+            timeStampSec: 0,
+            stepDurationInSec: 0
+          }],
+          workoutStepTimeArrayIndex: 1,
           workoutSteps: [
             'dynamic stretches',
             'bodyline drills'
@@ -44,17 +60,34 @@ export default {
     methods: {
       onTap () {
         this.currentStep++
-        this.workoutStart()
+        if(!this.totalWorkoutStarted) {
+          this.workoutStart()
+          this.totalWorkoutStarted = !this.totalWorkoutStarted
+        } else {
+          // this.workoutStop()
+          // this.totalWorkoutStarted = !this.totalWorkoutStarted
+        }
+
         // PRZEPISZ TO NA TIMEOUTY
       },
-      toReadableTime(milisec) {
-        const minutes = Math.floor(milisec / 60000)
-        const seconds = ((milisec % 60000) /1000)
-        const outSeconds = seconds.toString().split('.')[0]
-        return (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + outSeconds
+      onBreakStart () {
+        this.breakStarted = true
+      },
+      onNextWorkoutStep () {
+        this.workoutStepTimeArray[this.workoutStepTimeArrayIndex] = {
+          timeStampSec: (this.totalWorkoutMinutes * 60) + this.totalWorkoutSeconds,
+          stepDurationInSec: (this.totalWorkoutMinutes * 60) + this.totalWorkoutSeconds - this.workoutStepTimeArray[this.workoutStepTimeArrayIndex - 1].timeStampSec
+        }
+        this.workoutStepTimeArrayIndex++
+        console.log(this.workoutStepTimeArray)
+        console.log(this.workoutStepTimeArray[this.workoutStepTimeArrayIndex - 1].stepDurationInSec)
       },
       workoutStart () {
-        this.totalWorkoutTimeout = setInterval(this.totalWorkoutTimeAdder,1000)
+        // this.totalWorkoutInterval = setInterval(this.totalWorkoutTimeAdder,1000)
+        this.totalWorkoutInterval = setInterval(this.oneSecInterval,1000)
+      },
+      workoutStop () {
+        clearInterval(this.totalWorkoutInterval)
       },
       totalWorkoutTimeAdder () {
         this.totalWorkoutSeconds++
@@ -63,11 +96,38 @@ export default {
           this.totalWorkoutMinutes++
         }
         this.totalWorkoutTimeDisplay()
-        console.log('halko')
+      },
+      oneSecInterval () {
+        if(this.totalWorkoutStarted) {
+          this.totalWorkoutTimeAdder()
+        }
+
+        if(this.breakStarted) {
+          this.breakTimeSubstractor()
+        }
+      },
+      breakTimeSubstractor () {
+        this.breakTimeSeconds--
+        if (this.breakTimeSeconds === 0 && this.breakTimeMinutes === 0) {
+          console.log('break over!')
+          this.breakTimeSeconds = 5
+          this.breakTimeMinutes = 0
+          this.breakStarted = false
+        }
+        if (this.breakTimeSeconds <= 0) {
+          this.breakTimeSeconds = 60
+          this.breakTimeMinutes--
+        }
+        this.breakTimeDisplayer()
       },
       totalWorkoutTimeDisplay () {
         this.totalWorkoutTime =  (this.totalWorkoutMinutes < 10 ? '0' : '') + this.totalWorkoutMinutes + ':' + (this.totalWorkoutSeconds < 10 ? '0' : '') + this.totalWorkoutSeconds
-        // this.totalWorkoutTime = (this.totalWorkoutMinutes < 10 ? '0' : '') +  this.totalWorkoutMinutes + ':' (this.totalWorkoutSeconds < 10 ? '0' : '') + this.totalWorkoutSeconds
+      },
+      breakTimeDisplayer () {
+        this.breakTimeDisplay = (this.breakTimeMinutes < 10 ? '0' : '') + this.breakTimeMinutes + ':' + (this.breakTimeSeconds < 10 ? '0' : '') + this.breakTimeSeconds
+      },
+      halkor () {
+        console.log('halkor')
       }
     }
 };
